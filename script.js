fee8d9453e9e8012c65049186271b83107cc8327
zyqunix <fabiovanek2008@gmail.com>
2024-10-19 16:38:38 +0200

    ggserg

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-const rmbReset = document.getElementById('rmbReset')
-const lmbReset = document.getElementById('lmbReset')
+const lmbReset = document.getElementById('lmbReset');
+const rmbReset = document.getElementById('rmbReset');
 
 let leftCount = 0;
 let rightCount = 0;
 
-
 document.addEventListener('click', function(event) {
-    if (event.button === 0) {
+    if (event.button === 0 && event.target.tagName !== 'BUTTON') {
         leftCount++;
         document.getElementById('leftClickCount').innerText = leftCount;
     }
 });
 
 document.addEventListener('contextmenu', function(event) {
-    event.preventDefault(); // Disable right-click context menu
-    rightCount++;
-    document.getElementById('rightClickCount').innerText = rightCount;
+    event.preventDefault();
+    if (event.target.tagName !== 'BUTTON') {
+        rightCount++;
+        document.getElementById('rightClickCount').innerText = rightCount;
+    }
+});
+
+lmbReset.addEventListener('click', function() {
+    leftCount = 0;
+    document.getElementById('leftClickCount').innerText = leftCount;
 });
 
 rmbReset.addEventListener('click', function() {
-    rightCount = -1;
-    document.getElementById('clickCount').innerText = count;
-})
-
-lmbReset.addEventListener('click', function() {
-    leftCount = -1;
-    document.getElementById('clickCount').innerText = count;
-})
+    rightCount = 0;
+    document.getElementById('rightClickCount').innerText = rightCount;
+});
